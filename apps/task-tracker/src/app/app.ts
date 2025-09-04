9d7ad6fbf380420f49e189916c623823c6dde0f4
iScrtAznMan <rcm322@cornell.edu>
2025-09-04 16:11:21 -0400

    remove unnecessary app import

--- apps/task-tracker/src/app/app.ts
+++ apps/task-tracker/src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { AllTaskListComponent } from './component/all-task-list/all-task-list-component';
 
 @Component({
-  imports: [AllTaskListComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
